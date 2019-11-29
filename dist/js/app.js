427d207fb71d16a0fa09850761e9f21d314494c8
larrysul <suleabimbola@gmail.com>
2019-11-29 04:31:44 +0100

    Image Transistions Using JavaScript

---
+++ dist/js/app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.3,
+    image1: './dist/img/3.jpg',
+    image2: './dist/img/4.jpg',
+    displacementImage: './dist/img/heightMap.png'
+}); 
